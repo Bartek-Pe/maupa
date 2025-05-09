367c8e5bf37e7c3f1e0c315377160934be30d08a
Bartek Pierr <bartek.pierr@gmail.com>
2025-05-09 02:00:56 +0200

    fixing empty components

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1>DEFAULT</h1>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+    h1 {
+        font-size: 2.5rem;
+        margin-bottom: 1rem;
+    }
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1>ABOUT</h1>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+    h1 {
+        font-size: 2.5rem;
+        margin-bottom: 1rem;
+    }
+</style>
